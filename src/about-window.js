a3952b0d1c3890b8dfdf52f49982d438d5fedcb2
TANIGUCHI Masaya <ta2gch@moffice.space>
2017-04-05 19:46:28 +0900

    change description in about-window

--- src/about-window.js
+++ src/about-window.js
@@ -9,7 +9,7 @@ function showAboutWindow(){
             copyright: "Copyright (c) 2017 TANIGUCHI Masaya",
             bug_report_url: "https://github.com/ta2gch/yomu/issues",
             homepage: "https://ta2gch.github.com/yomu",
-            description: "和英辞書内蔵PDF表示ソフト",
+            description: "英和辞書内蔵PDF表示ソフト",
             license: "Apache License 2.0"
         });
     } else {
